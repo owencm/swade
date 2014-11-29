d4d5e01ed42987a0a07840c019be18f04d2ed02a
Victor Ionescu <vvv@fb.com>
2014-11-29 22:37:58 +0000

    [model] Added model for subcategories.

--- swade_on_rails/db/schema.rb
+++ swade_on_rails/db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129221317) do
+ActiveRecord::Schema.define(version: 20141129221926) do
 
   create_table "categories", force: true do |t|
     t.string   "name",       null: false
     t.string   "shortname",  null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "subcategories", force: true do |t|
+    t.string   "name",        null: false
+    t.string   "shortname",   null: false
+    t.integer  "category_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
