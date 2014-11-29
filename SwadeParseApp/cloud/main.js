bcf9c9607ff0241ec13c18728dbe2d29c9ee7397
Emil <eculic17@gmail.com>
2014-11-29 22:10:41 +0000

    [scraper] scraping colors for each subCat.

--- SwadeParseApp/cloud/main.js
+++ SwadeParseApp/cloud/main.js
@@ -1,9 +1,9 @@
-var rootURL = 'http://www.matchesfashion.com/mens/shop/';
+var rootURL = 'http://www.matchesfashion.com/mens';
 var queryURL = '?type=&orderby=&pagesize=240';
 
 var categories = [
 	{
-		name 			: 'blazers',
+		name 			: 'shop/blazers',
 		subCategories 	: [
 			'casual',
 			'formal'
@@ -79,6 +79,8 @@ Parse.Cloud.job("scrapeJob", function(request, status) {
 
 	console.log("Beginning scrape job");
 
+	var pageScrapePromises = [];
+
 	for (var categoryIndex in categories) {
 		var category = categories[categoryIndex];
 		var categoryName = category.name;
@@ -88,8 +90,62 @@ Parse.Cloud.job("scrapeJob", function(request, status) {
 			subCategoryName = category.subCategories[subCategoryIndex];
 			console.log("Scraping category: " + categoryName + " sub-category: " + subCategoryName);
 
+			pageScrapePromises.push(scrapePage(categoryName, subCategoryName));
 		}
 	}
 
-	status.success('Scraping done.');
+	Parse.Promise.when(pageScrapePromises)
+	.then(
+		function() {
+			status.success('Scraping done.');
+		},
+		function() {
+			status.error('An page has occured.');
+		}
+	);
 });
+
+function scrapePage(categoryName, subCategoryName) {
+	var urlString = rootURL + '/' + categoryName + '/' + subCategoryName + queryURL;
+
+	var pageScrapePromise = Parse.Cloud.httpRequest({
+		url: urlString
+	})
+	.then(
+		function (httpResponse) {
+			var colors = extractColors(httpResponse.text);
+			console.log(categoryName + " " + subCategoryName + " " + colors);
+		},
+		function () {
+			console.log("error occured scraping " + URL);
+		}
+	);
+
+	return pageScrapePromise;
+}
+
+function extractColors(s) {
+	var colors = [];
+	
+	var colourSpanTag = '<span>Colour</span>';
+	s = s.substring(s.indexOf(colourSpanTag));
+	
+	var startIndex = s.indexOf('<ul class="scroll">');
+	var endIndex = s.indexOf('</ul>')
+	s = s.substring(startIndex, endIndex);	
+
+	startIndex = s.indexOf(': Colour">');
+	// find all li elements
+	while (startIndex >= 0) {
+		// add url
+		colors.push(s.substring(startIndex + 10, s.indexOf("</a>")));
+
+		// advance to end of li tag
+		s = s.substring(s.indexOf("</li>") + 5);
+		
+		//find start of next color li
+		startIndex = s.indexOf(': Colour">');
+	}
+
+	return colors;
+}
